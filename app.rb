6f60e3b3cc491eacccc12ff921839aafbb0999fc
obfuscurity <jason@dixongroup.net>
2014-11-15 21:19:45 -0500

    use event name in uri, more debug output

--- app.rb
+++ app.rb
@@ -11,23 +11,31 @@ module Libratito
     end
 
     before do
-      halt unless validated?
       content_type :json
     end
 
     helpers do
-      def validated?
-        # check TITO_WEBHOOK_TOKEN here
-        return true
+      def validates?(secret)
+        ENV['TITO_WEBHOOK_TOKEN'].eql?(secret)
       end
     end
 
-    post '/event/?' do
-      p JSON.parse(request.body.read)
-      # check request.env['HTTP_X_WEBHOOK_NAME'] for 'ticket.created' or 'ticket.updated'
+    post '/events/:name?' do
+      source = params[:name]
+      prefix = 'libratito.'
+
+      tito_user_action = request.env['HTTP_X_WEBHOOK_NAME'] # ticket.{created,updated}
+      tito_webhook_data = JSON.parse(request.body.read)
+
+      halt unless validates?(tito_webhook_data['custom'])
+
+      puts "source is #{source}"
+      puts "user action is #{tito_user_action}"
+      puts "webhook data is:"
+      p tito_webhook_data
+
       #RestClient.post 'https://metrics-api.librato.com/v1/metrics'
       #RestClient.post 'https://metrics-api.librato.com/v1/annotations/monitorama-registration'
-      # XXX use 'pdx2015' as the source
       200
     end
 
