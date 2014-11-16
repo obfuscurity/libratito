5e300c989bb86ef6861ee1f1609c47c106a40050
obfuscurity <jason@dixongroup.net>
2014-11-15 21:25:49 -0500

    more debugging

--- app.rb
+++ app.rb
@@ -24,15 +24,23 @@ module Libratito
       source = params[:name]
       prefix = 'libratito.'
 
+      p '1'
+      p JSON.parse(request.body.read)
+      p '2'
       tito_user_action = request.env['HTTP_X_WEBHOOK_NAME'] # ticket.{created,updated}
+      p '3'
       tito_webhook_data = JSON.parse(request.body.read)
+      p '4'
 
       halt unless validates?(tito_webhook_data['custom'])
+      p '5'
 
       puts "source is #{source}"
       puts "user action is #{tito_user_action}"
       puts "webhook data is:"
+      p '6'
       p tito_webhook_data
+      p '7'
 
       #RestClient.post 'https://metrics-api.librato.com/v1/metrics'
       #RestClient.post 'https://metrics-api.librato.com/v1/annotations/monitorama-registration'
