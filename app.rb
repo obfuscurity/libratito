8252876db5f667f3f0e9a6ee4ef7135f68ea461f
obfuscurity <jason@dixongroup.net>
2014-11-15 23:26:55 -0500

    combine into a single metric type

--- app.rb
+++ app.rb
@@ -35,14 +35,25 @@ module Libratito
       authenticate_to_librato
 
       queue = Librato::Metrics::Queue.new
-      queue.add "#{prefix}.#{tito_user_action}" => { :source => source, :value => 1 }
-      queue.add "#{prefix}.type.#{tito_data['release'].gsub(/\s+/, '_')}" => { :source => source, :value => 1 }
-      queue.add "#{prefix}.price" => { :source => source, :value => tito_data['price'].to_f }
+      queue.add "#{prefix}.price" => {
+        :source => source,
+        :value => tito_data['price'].to_f
+      }
+      queue.add "#{prefix}.type.#{tito_data['release'].gsub(/\s+/, '_')}.#{tito_user_action}" => {
+        :source => source,
+        :value => 1
+      }
       if !tito_data['release_price'].nil?
-        queue.add "#{prefix}.release_price" => { :source => source, :value => tito_data['release_price'].to_f }
+        queue.add "#{prefix}.release_price" => {
+          :source => source,
+          :value => tito_data['release_price'].to_f
+        }
       end
       if !tito_data['discount_code_used'].empty?
-        queue.add "#{prefix}.discount_code.#{tito_data['discount_code_used']}" => { :source => source, :value => 1 }
+        queue.add "#{prefix}.discount_code.#{tito_data['discount_code_used']}" => {
+          :source => source,
+          :value => 1
+        }
       end
       queue.submit
 
