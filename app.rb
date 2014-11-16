6a7e3245ceac75f0d80ace14ff1b08b621b4f6df
obfuscurity <jason@dixongroup.net>
2014-11-15 21:33:14 -0500

    grab request payload before

--- app.rb
+++ app.rb
@@ -12,6 +12,8 @@ module Libratito
 
     before do
       content_type :json
+      request.body.rewind
+      @request_payload = JSON.parse(request.body.read)
     end
 
     helpers do
@@ -25,22 +27,19 @@ module Libratito
       prefix = 'libratito.'
 
       p '1'
-      p JSON.parse(request.body.read)
-      p '2'
       tito_user_action = request.env['HTTP_X_WEBHOOK_NAME'] # ticket.{created,updated}
+      p '2'
+      tito_webhook_data = @request_payload
       p '3'
-      tito_webhook_data = JSON.parse(request.body.read)
-      p '4'
-
       halt unless validates?(tito_webhook_data['custom'])
-      p '5'
+      p '4'
 
       puts "source is #{source}"
       puts "user action is #{tito_user_action}"
       puts "webhook data is:"
-      p '6'
+      p '5'
       p tito_webhook_data
-      p '7'
+      p '6'
 
       #RestClient.post 'https://metrics-api.librato.com/v1/metrics'
       #RestClient.post 'https://metrics-api.librato.com/v1/annotations/monitorama-registration'
